61221b73bbcf7d4eb82038c99110a5efd232ca98
Nicolas Perez <nicolas_perez@brown.edu>
2024-10-03 11:01:17 -0400

    chore: Cleaned up config file

--- passive_sound_localization/config/config.py
+++ passive_sound_localization/config/config.py
@@ -1,8 +1,14 @@
-from dataclasses import dataclass
-from configs.feature_flags_config import FeatureFlagsConfig
-from configs.logging_config import LoggingConfig
+from dataclasses import dataclass, field
+from hydra.core.config_store import ConfigStore
+
+from config.feature_flags_config import FeatureFlagsConfig
+from config.logging_config import LoggingConfig
 
 @dataclass
 class Config:
-    feature_flags: FeatureFlagsConfig = FeatureFlagsConfig()
-    logging: LoggingConfig = LoggingConfig()
+    feature_flags: FeatureFlagsConfig = field(default_factory=FeatureFlagsConfig)
+    logging: LoggingConfig = field(default_factory=LoggingConfig)
+
+
+cs = ConfigStore.instance()
+cs.store(name="config", node=Config)
