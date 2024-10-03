ee1f6b1acae0b097313a09951e120c3fb7ab61f9
Nicolas Perez <nicolas_perez@brown.edu>
2024-10-03 11:00:34 -0400

    chore: Cleaned up main2 file

--- passive_sound_localization/main2.py
+++ passive_sound_localization/main2.py
@@ -1,20 +1,15 @@
 import hydra
-from hydra.core.config_store import ConfigStore
-from configs.config import Config
 import logging
 
+from config.config import Config
 from logger import setup_logger
 
-
-cs = ConfigStore.instance()
-cs.store(name="config", node=Config)
-
 @hydra.main(version_base=None, config_path=".", config_name="config")
-def main(cfg: Config):
+def main(cfg: Config) -> None:
     # Setup logger
     setup_logger(cfg.logging, cfg.feature_flags.enable_logging)
     logger = logging.getLogger(__name__)
-    logger.info("Starting main script.")
+    logger.info("Running main2.py...")
 
 if __name__ == "__main__":
     main()
