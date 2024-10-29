d19a0aac908e16df25fc7d8536f9a51afd07cdb1
Nicolas Perez <nicolas_perez@brown.edu>
2024-10-29 14:47:15 -0400

    feat: Finished sync openai websocket client

---
+++ passive_sound_localization/passive_sound_localization/models/configs/openai_websocket.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+@dataclass(frozen=True)
+class OpenAIWebsocketConfig:
+    api_key: str = os.getenv(
+        "OPENAI_API_KEY", ""
+    )  # Replace with your actual OpenAI API key
+    websocket_url: str = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01"  # The name of the OpenAI Websocket url,
