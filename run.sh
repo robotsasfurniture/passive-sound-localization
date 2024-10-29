4f102b2ddf1c77b91cfec55731cb7a174808dda2
John Finberg <johnbfinberg@gmail.com>
2024-10-29 16:08:38 -0400

    fix: copying all packages in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 git pull
 cd ../
-cp -rf passive-sound-localization/passive_sound_localization ros2_ws/src/
+cp -rf passive-sound-localization/packages ros2_ws/src/
 cd ros2_ws/src
-colcon build --packages-select passive_sound_localization
-source install/setup.bash
-ros2 launch passive_sound_localization passive_sound_localization.launch.py
+colcon build
