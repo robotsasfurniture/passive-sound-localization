b5e2159d46aeb7bc8a15fe8be0dd3924f4966765
Nicolas Perez <nicolas_perez@brown.edu>
2024-10-31 15:10:04 -0400

    feat: Added sanity check that localization and realtime audio streamer sample rates and chunk sizes match in config

--- packages/passive_sound_localization/passive_sound_localization/models/configs/__init__.py
+++ packages/passive_sound_localization/passive_sound_localization/models/configs/__init__.py
@@ -40,6 +40,24 @@ class Config:
     realtime_streamer: RealtimeAudioStreamerConfig = field(default_factory=RealtimeAudioStreamerConfig)
 
     def build_configs(self) -> "Config":
+        def check_chunk_sizes() -> int:
+            localization_fft_size = self.get_parameter("localization.fft_size").value
+            realtime_streamer_chunk = self.get_parameter("realtime_streamer.chunk").value
+
+            if localization_fft_size != realtime_streamer_chunk:
+                raise ValueError("Localization FFT size and RealtimeAudioStreamer chunk size should match")
+            
+            return localization_fft_size
+
+        def check_sample_rates() -> int:
+            localization_sample_rate = self.get_parameter("localization.sample_rate").value
+            realtime_streamer_sample_rate = self.get_parameter("realtime_streamer.sample_rate").value
+
+            if localization_sample_rate != realtime_streamer_sample_rate:
+                raise ValueError("Localization and RealtimeAudioStreamer sample rates should match")
+            
+            return localization_sample_rate
+        
         def calculate_mic_positions() -> list[list[float]]:
             mic_array_x = self.get_parameter("localization.mic_array_x").value
             mic_array_y = self.get_parameter("localization.mic_array_y").value
@@ -54,8 +72,8 @@ class Config:
                 speed_of_sound=self.get_parameter("localization.speed_of_sound").value,
                 mic_distance=self.get_parameter("localization.mic_distance").value
                 / 100,
-                sample_rate=self.get_parameter("localization.sample_rate").value,
-                fft_size=self.get_parameter("localization.fft_size").value,
+                sample_rate=check_sample_rates(),
+                fft_size=check_chunk_sizes(),
                 mic_positions=calculate_mic_positions(),
             ),
             logging=LoggingConfig(
@@ -72,9 +90,9 @@ class Config:
                 instructions=self.get_parameter("openai_websocket.instructions").value,
             ),
             realtime_streamer=RealtimeAudioStreamerConfig(
-                sample_rate=self.get_parameter("realtime_streamer.sample_rate").value,
+                sample_rate=check_sample_rates(),
                 channels=self.get_parameter("realtime_streamer.channels").value,
-                chunk=self.get_parameter("realtime_streamer.chunk").value,
+                chunk=check_chunk_sizes(),
                 device_indices=self.get_parameter("realtime_streamer.device_indices").value
             )
         )
