cf77dd05e41b5edf069d3004c6bffac5ac06a4b1
Jitpuwapat Mokkamakkul <jitpuwapat_mokkamakkul@brown.edu>
2024-11-02 13:01:16 -0400

    movement library package updated

--- packages/movement_library/movement_library/main.py
+++ packages/movement_library/movement_library/main.py
@@ -3,125 +3,111 @@ from passive_sound_localization.logger import setup_logger
 from rclpy.node import Node
 import logging
 import rclpy
+import math
 
+from std_msgs.msg import Byte
+from std_msgs.msg import Float32
+from std_msgs.msg import Int32
 
-# class LocalizationNode(Node):
-#     def __init__(self):
-#         super().__init__("localization_node")
-#         self.declare_parameters(
-#             namespace="",
-#             parameters=[
-#                 ("audio_mixer.sample_rate", 16000),
-#                 ("audio_mixer.chunk_size", 1024),
-#                 ("audio_mixer.record_seconds", 5),
-#                 ("audio_mixer.mic_count", 4),
-#                 ("vad.enabled", True),
-#                 ("vad.aggressiveness", 2),
-#                 ("vad.frame_duration_ms", 30),
-#                 ("transcriber.api_key", ""),
-#                 ("transcriber.model_name", "whisper-1"),
-#                 ("transcriber.language", "en"),
-#                 ("localization.speed_of_sound", 343.0),
-#                 ("localization.sample_rate", 16000),
-#                 ("localization.fft_size", 1024),
-#                 ("localization.mic_array_x", [0.00]),
-#                 ("localization.mic_array_y", [0.00]),
-#                 ("localization.mic_distance", 0.05),
-#                 ("localization.angle_resolution", 1),
-#                 ("logging.level", "INFO"),
-#                 ("feature_flags.enable_logging", True),
-#                 ("openai_websocket.api_key", ""),
-#                 (
-#                     "openai_websocket.websocket_url",
-#                     "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01",
-#                 ),
-#                 ("openai_websocket.instructions", ""),
-#             ],
-#         )
+from geometry_msgs.msg import Twist
 
-#         self.config: Config = Config.build_configs(self)
+from example_interfaces.msg import Bool
+from passive_sound_localization_msgs.msg import LocalizationResult
 
-#         # Setup logger
-#         setup_logger(self.config.logging, self.config.feature_flags.enable_logging)
-#         self.logger = logging.getLogger(__name__)
-#         self.logger.info("Running Localization Node")
+class SimpleHagridDemoMic(Node):
+    def __init__(self):
+        super().__init__('simple_hagrid_demo')
+        self.cmd_vel_publisher=self.create_publisher(Twist,'rcm/cmd_vel',1)
+        self.enable_publisher=self.create_publisher(Bool,'rcm/enabled',1)
 
-#         # Setup ROS publisher
-#         self.publisher = self.create_publisher(
-#             LocalizationResult, "localization_results", 10
-#         )
+        self.batterySubscription=self.create_subscription(Float32,'rcm/battery',self.battery_callback,1)
+        self.batterySubscription # prevent warning
 
-#         # Initialize components with configurations
-#         self.localizer = SoundLocalizer(self.config.localization)
-#         self.audio_mixer = AudioMixer(self.config.audio_mixer)
-#         self.streamer = RealtimeAudioStreamer(
-#             sample_rate=self.config.localization.sample_rate,
-#             channels=1,
-#             chunk=self.config.audio_mixer.chunk_size,
-#         )
-#         self.openai_ws_client = OpenAIWebsocketClient(self.config.openai_websocket)
+        # LOCALIZATION RESULT HERE... 
+        # self.create_subscription(LocalizationResult,'passive_sound_localization_msgs',1)
+        self.localizationSubscription={"distance": 0.91, "angle": 180}
+        # self.create_subscription(...)
 
-#         # Start processing audio
-#         self.process_audio()
+        self.loop_time_period=1.0/10.0
+        self.loop_timer=self.create_timer(self.loop_time_period,self.loop)
+        self.time=0.0
 
-#     def process_audio(self):
-#         self.logger.info("Processing audio...")
+        # Temporary boolean that is set to true after movement is finished...
+        # Do we want the robot to self-adjust to new vocal queues as it's moving?
+        self.executing=False
 
-#         with self.streamer as streamer, self.openai_ws_client as client:
-#             multi_channel_stream = streamer.multi_channel_gen()
-#             single_channel_stream = streamer.single_channel_gen()
+    def string_to_dict(self, msg):
+        ...
 
-#             # TODO: Clean up threading so the localization is properly integrated
-#             with ThreadPoolExecutor(max_workers=2) as executor:
-#                 executor.submit(send_audio_continuously, client, single_channel_stream)
-#                 executor.submit(receive_text_messages, client)
+    def battery_callback(self, msg):
+        self.get_logger().info('battery voltage "%d"' % msg.data)
 
-#             for audio_data in multi_channel_stream:
-#                 #  Stream audio data and pass it to the localizer
-#                 localization_stream = self.localizer.localize_stream(
-#                     [audio_data[k] for k in audio_data.keys()]
-#                 )
+    def calculate_time_xyz(self, distance, velocity):
+        return distance/(velocity*1.03)
+    
+    def calculate_time_ang(self, angle, ang_velocity):
+        radians = angle*math.pi/180
+        return radians/(ang_velocity*0.9881)
+    
+    def loop(self):
+        # something to stop time from incrementing or reset it when we get a new input
+        self.time=self.time+self.loop_time_period
 
-#                 for localization_results in localization_stream:
-#                     for result in localization_results:
-#                         self.logger.info(
-#                             f"Estimated source at angle: {result.angle} degrees, distance: {result.distance} meters"
-#                         )
-#                         coordinate_representation = (
-#                             self.localizer.compute_cartesian_coordinates(
-#                                 result.distance, result.angle
-#                             )
-#                         )
-#                         self.logger.info(
-#                             f"Cartesian Coordinates: x={coordinate_representation[0]}, y={coordinate_representation[1]}"
-#                         )
+        self.get_logger().info("time: %d"%(self.time))
 
-#                     # Publish results to ROS topic
-#                     self.publish_results(localization_results)
+        enableMsg=Bool()
+        enableMsg.data=True
+        self.enable_publisher.publish(enableMsg)
 
-#     def publish_results(self, localization_results):
-#         # Publish results to ROS topic
-#         msg = LocalizationResult()
-#         msg.angle = localization_results.angle
-#         msg.distance = localization_results.distance
-#         self.get_logger().info(
-#             f"Publishing: angle={msg.angle}, distance={msg.distance}"
-#         )
-#         self.publisher.publish(msg)
+        velocityMsg=Twist()
+        velocityMsg.linear.z=0.0
+        velocityMsg.angular.x=0.0
+        velocityMsg.angular.y=0.0
+        velocityMsg.linear.y=0.0
+        velocityMsg.angular.z=0.0
+        velocityMsg.linear.x=0.0
 
+        spdx = 0.3
+        spdang = 0.5
+        time_xyz = self.calculate_time_xyz(self.localizationSubscription["distance"], spdx)
+        time_ang = self.calculate_time_ang(self.localizationSubscription["angle"], spdang)
+        buff = 1
+        wait = 2
+        
+        time_ang = time_ang+wait
+        # print(time_xyz)
+        # print(time_ang)
+        # print(time_xyz+time_ang+buff)
+        
+        if self.time<=time_ang and self.time>wait:
+            velocityMsg.angular.z=spdang
+        else:
+            velocityMsg.angular.z=0.0
 
-# def main() -> None:
-#     rclpy.init()
-#     localization_node = LocalizationNode()
+        if self.time<=time_xyz+time_ang+buff and self.time>time_ang+buff:
+            velocityMsg.linear.x=spdx
 
-#     try:
-#         rclpy.spin(localization_node)
-#     except KeyboardInterrupt:
-#         localization_node.logger.info("Shutting down.")
-#     finally:
-#         localization_node.destroy_node()
-#         rclpy.shutdown()
+        if self.time>(time_xyz+time_ang+buff):
+            self.localizationSubscription["distance"]=0
+            self.localizationSubscription["angle"]=0
+            velocityMsg.linear.x=0.0
+            self.time=0  # not sure if needed     
+        
+        # print(velocityMsg.linear.x)
+        # print(velocityMsg.angular.z)
+        
+        self.cmd_vel_publisher.publish(velocityMsg)
+        
 
+def main(args=None):
+    rclpy.init(args=args)
 
-# if __name__ == "__main__":
-#     main()
+    simple_hagrid_demo=SimpleHagridDemoMic()
+
+    rclpy.spin(simple_hagrid_demo)
+
+    simple_hagrid_demo.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
