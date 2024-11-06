523b2b6aa26acda1f38e47a41afde87b19f77bb2
Nicolas Perez <nicolas_perez@brown.edu>
2024-11-06 14:13:26 -0500

    fix: Fixed bugs

--- packages/passive_sound_localization/passive_sound_localization/models/configs/realtime_streamer.py
+++ packages/passive_sound_localization/passive_sound_localization/models/configs/realtime_streamer.py
@@ -3,8 +3,8 @@ from typing import List
 
 @dataclass(frozen=True)
 class RealtimeAudioStreamerConfig:
-    sample_rate: int = 24000,
-    channels: int = 1,
+    sample_rate: int = 24000
+    channels: int = 1
     chunk: int = 1024
     device_indices: List[int] = field(default_factory=lambda: [2, 3, 4, 5]) # Lab configuration
     # device_indices: List[int] = field(default_factory=lambda: [4, 6, 8, 10]) # Nico's laptop (configuration 1)
