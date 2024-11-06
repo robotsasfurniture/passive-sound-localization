55fdf7a9449d592ce6297b6a0a00265aee204be1
Jitpuwapat Mokkamakkul <jitpuwapat_mokkamakkul@brown.edu>
2024-11-06 14:27:56 -0500

    synched?

--- packages/movement_library/movement_library/main.py
+++ packages/movement_library/movement_library/main.py
@@ -42,7 +42,7 @@ class MovementNode(Node):
         self.executing = False
         self.logger = logging.getLogger(__name__)
 
-    def string_to_dict(self, msg): ...
+    # def string_to_dict(self, msg): ...
 
     def battery_callback(self, msg):
         self.logger.info('battery voltage "%d"' % msg.data)
@@ -56,59 +56,65 @@ class MovementNode(Node):
 
     def localizer_callback(self, msg):
         self.logger.info("Got a message")
-        angle = msg.angle
+        angle = 360-msg.angle
         distance = msg.distance
+        if self.executing:
+            pass
+        else:
+            self.localizationSubscription={"distance": distance, "angle": angle}
+            self.time = 0.0
+            self.executing = True
         self.logger.info(f"Got {str(angle)} {str(distance)}")
 
     def loop(self):
         # something to stop time from incrementing or reset it when we get a new input
-        # self.time=self.time+self.loop_time_period
+        self.time=self.time+self.loop_time_period
 
-        # self.get_logger().info("time: %d"%(self.time))
+        self.get_logger().info("time: %d"%(self.time))
 
-        # enableMsg=Bool()
-        # enableMsg.data=True
-        # self.enable_publisher.publish(enableMsg)
+        enableMsg=Bool()
+        enableMsg.data=True
+        self.enable_publisher.publish(enableMsg)
 
-        # velocityMsg=Twist()
-        # velocityMsg.linear.z=0.0
-        # velocityMsg.angular.x=0.0
-        # velocityMsg.angular.y=0.0
-        # velocityMsg.linear.y=0.0
-        # velocityMsg.angular.z=0.0
-        # velocityMsg.linear.x=0.0
+        velocityMsg=Twist()
+        velocityMsg.linear.z=0.0
+        velocityMsg.angular.x=0.0
+        velocityMsg.angular.y=0.0
+        velocityMsg.linear.y=0.0
+        velocityMsg.angular.z=0.0
+        velocityMsg.linear.x=0.0
 
-        # spdx = 0.3
-        # spdang = 0.5
-        # time_xyz = self.calculate_time_xyz(self.localizationSubscription["distance"], spdx)
-        # time_ang = self.calculate_time_ang(self.localizationSubscription["angle"], spdang)
-        # buff = 1
-        # wait = 2
+        spdx = 0.3
+        spdang = 0.5
+        time_xyz = self.calculate_time_xyz(self.localizationSubscription["distance"], spdx)
+        time_ang = self.calculate_time_ang(self.localizationSubscription["angle"], spdang)
+        buff = 1
+        wait = 2
 
-        # time_ang = time_ang+wait
-        # # print(time_xyz)
-        # # print(time_ang)
-        # # print(time_xyz+time_ang+buff)
+        time_ang = time_ang+wait
+        # print(time_xyz)
+        # print(time_ang)
+        # print(time_xyz+time_ang+buff)
 
-        # if self.time<=time_ang and self.time>wait:
-        #     velocityMsg.angular.z=spdang
-        # else:
-        #     velocityMsg.angular.z=0.0
+        if self.time<=time_ang and self.time>wait:
+            velocityMsg.angular.z=spdang
+        else:
+            velocityMsg.angular.z=0.0
 
-        # if self.time<=time_xyz+time_ang+buff and self.time>time_ang+buff:
-        #     velocityMsg.linear.x=spdx
+        if self.time<=time_xyz+time_ang+buff and self.time>time_ang+buff:
+            velocityMsg.linear.x=spdx
 
-        # if self.time>(time_xyz+time_ang+buff):
-        #     self.localizationSubscription["distance"]=0
-        #     self.localizationSubscription["angle"]=0
-        #     velocityMsg.linear.x=0.0
-        #     self.time=0  # not sure if needed
+        if self.time>(time_xyz+time_ang+buff):
+            self.localizationSubscription["distance"]=0
+            self.localizationSubscription["angle"]=0
+            velocityMsg.linear.x=0.0
+            self.time=0  # not sure if needed
+            self.executing=False
 
-        # # print(velocityMsg.linear.x)
-        # # print(velocityMsg.angular.z)
+        # print(velocityMsg.linear.x)
+        # print(velocityMsg.angular.z)
 
-        # self.cmd_vel_publisher.publish(velocityMsg)
-        pass
+        self.cmd_vel_publisher.publish(velocityMsg)
 
 
 def main(args=None):
@@ -124,3 +130,61 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
+
+
+# class MoveRobot(Node):
+#     def __init__(self):
+#         super().__init__('move_robot_node')
+        
+#         # Subscriber to get data (e.g., sensor distance)
+#         self.subscription = self.create_subscription(
+#             Float32,  # Adjust type based on your topic
+#             'sensor_topic',  # Replace with the actual topic name
+#             self.sensor_callback,
+#             10)
+        
+#         # Publisher to control the robot
+#         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        
+#         # Initializing a timer for continuous control updates
+#         timer_period = 0.1  # seconds
+#         self.timer = self.create_timer(timer_period, self.update_control)
+
+#         # Store sensor data and command variables
+#         self.sensor_data = 0.0
+#         self.move_cmd = Twist()
+
+#     def sensor_callback(self, msg):
+#         # Update sensor data when a new message arrives
+#         self.sensor_data = msg.data
+#         self.get_logger().info(f'Received sensor data: {self.sensor_data}')
+
+#     def update_control(self):
+#         # Process the sensor data and update the Twist message
+#         if self.sensor_data < 1.0:
+#             # Stop if too close to an object
+#             self.move_cmd.linear.x = 0.0
+#             self.move_cmd.angular.z = 0.0
+#         else:
+#             # Move forward if there's enough space
+#             self.move_cmd.linear.x = 0.5
+#             self.move_cmd.angular.z = 0.0
+
+#         # Publish the movement command
+#         self.publisher.publish(self.move_cmd)
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     move_robot_node = MoveRobot()
+    
+#     try:
+#         rclpy.spin(move_robot_node)
+#     except KeyboardInterrupt:
+#         pass
+    
+#     # Shutdown and cleanup
+#     move_robot_node.destroy_node()
+#     rclpy.shutdown()
+
+# if __name__ == '__main__':
+#     main()
