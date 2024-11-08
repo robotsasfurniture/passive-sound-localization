43c5f0c594df368fcbfaacb48beae17ac9c7e7d1
John Finberg <johnbfinberg@gmail.com>
2024-11-07 20:24:15 -0500

    refactor: from pyaudio to soundcard

--- packages/passive_sound_localization/passive_sound_localization/realtime_audio_streamer.py
+++ packages/passive_sound_localization/passive_sound_localization/realtime_audio_streamer.py
@@ -1,12 +1,18 @@
 import logging
 from typing import Dict, Generator, List, Optional
-from pyaudio import PyAudio, paInt16, Stream
+import soundcard as sc
 import numpy as np
 
-from passive_sound_localization.models.configs.realtime_streamer import RealtimeAudioStreamerConfig
-# from models.configs.realtime_streamer import RealtimeAudioStreamerConfig # Only needed to run with `realtime_audio.py`
+# from passive_sound_localization.models.configs.realtime_streamer import (
+#     RealtimeAudioStreamerConfig,
+# )
+
+from models.configs.realtime_streamer import (
+    RealtimeAudioStreamerConfig,
+)  # Only needed to run with `realtime_audio.py`
 
 logger = logging.getLogger(__name__)
+
 
 class RealtimeAudioStreamer:
     def __init__(self, config: RealtimeAudioStreamerConfig):
@@ -14,77 +20,56 @@ class RealtimeAudioStreamer:
         self.channels: int = config.channels
         self.chunk: int = config.chunk
         self.device_indices = config.device_indices
-        self.format = paInt16
-        self.audio: Optional[PyAudio] = None
-        self.streams: List[Optional[Stream]] = []
+        self.streams: Dict[int, np.ndarray] = {}
         self.streaming: bool = False
 
         print(self.device_indices)
 
     def __enter__(self):
-        self.audio = PyAudio()
-
-        self.streams = [
-            self.audio.open(
-                format=self.format,
+        microphones: List[sc._Microphone] = sc.all_microphones()
+        self.streams = {
+            device_index: microphones[device_index].record(
+                samplerate=self.sample_rate,
                 channels=self.channels,
-                rate=self.sample_rate,
-                input=True,
-                input_device_index=device_index,
-                frames_per_buffer=self.chunk,
+                numframes=self.chunk,
             )
             for device_index in self.device_indices
-        ]
+        }
         self.streaming = True
         return self
 
     def __exit__(self, *args):
         self.streaming = False
-        for stream in self.streams:
-            if stream:
-                stream.stop_stream()
-                stream.close()
-        self.streams = []
 
-        if self.audio:
-            self.audio.terminate()
-            self.audio = None
-
-    def _mix_audio_chunks(self, audio_arrays: List[np.ndarray]) -> np.ndarray:
-        if not audio_arrays:
-            return np.array([], dtype=np.int16)
-        mixed_data = np.sum(audio_arrays, axis=0) / len(audio_arrays)
-        mixed_data = np.clip(mixed_data, -32768, 32767).astype(np.int16)
-        return mixed_data
+    def get_stream(self, device_index: int) -> Optional[bytes]:
+        """Retrieve the audio stream for a specific device index."""
+        if device_index in self.device_indices:
+            return self.streams[device_index].tobytes()
+        else:
+            print(f"Device index {device_index} not found.")
+            return None
 
     def multi_channel_gen(self) -> Generator[Dict[int, bytes], None, None]:
         try:
             while self.streaming:
-                audio_data = {}
-                for device_index, stream in zip(self.device_indices, self.streams):
-                    try:
-                        data = stream.read(self.chunk, exception_on_overflow=False)
-                        audio_data[device_index] = data
-                    except Exception as e:
-                        print(f"Error reading from device {device_index}: {e}")
-                if audio_data:
-                    yield audio_data
+                audio_arrays = []
+                for device_index in self.device_indices:
+                    audio_arrays.append(self.get_stream(device_index))
+
+                yield {
+                    device_index: audio
+                    for device_index, audio in zip(self.device_indices, audio_arrays)
+                }
+
         except Exception as e:
-            print(f"Error in audio_generator: {e}")
+            print(f"Error in multi_channel_gen: {e}")
+
+    def merge_streams(self, streams: List[np.ndarray]) -> np.ndarray:
+        return np.sum(streams, axis=0) / len(streams)
 
     def single_channel_gen(self) -> Generator[bytes, None, None]:
         try:
             while self.streaming:
-                audio_arrays = []
-                for device_index, stream in zip(self.device_indices, self.streams):
-                    try:
-                        data = stream.read(self.chunk, exception_on_overflow=False)
-                        audio_array = np.frombuffer(data, dtype=np.int16)
-                        audio_arrays.append(audio_array)
-                    except Exception as e:
-                        print(f"Error reading from device {device_index}: {e}")
-                if audio_arrays:
-                    mixed_data = self._mix_audio_chunks(audio_arrays)
-                    yield mixed_data.tobytes()
+                yield self.merge_streams(list(self.streams.values())).tobytes()
         except Exception as e:
-            print(f"Error in mixed_audio_generator: {e}")
+            print(f"Error in single_channel_gen: {e}")
