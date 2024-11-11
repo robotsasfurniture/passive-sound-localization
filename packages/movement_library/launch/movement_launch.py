fc51c1dfb5fec709a0cdf2be81629e314ffe4a15
Jitpuwapat Mokkamakkul <92137687+earth15354@users.noreply.github.com>
2024-11-11 15:09:40 -0500

    feat: movement library based on callback function (#18)
    * adding skeleton for movement library
    
    * movement library package updated
    
    * code from john/machine-code-sync

---
+++ packages/movement_library/launch/movement_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="movement_library",
+                executable="movement_node",
+                name="movement_node",
+                output="screen",
+            )
+        ]
+    )
