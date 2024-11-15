18a2d0e2de2a996b7e83353acedc04a423e863f8
Nicolas Perez <nicolas_perez@brown.edu>
2024-11-15 17:06:22 -0500

    feat: Added testing file to test localization init

--- packages/passive_sound_localization/passive_sound_localization/test_audio_streamer.py
+++ packages/passive_sound_localization/passive_sound_localization/test_audio_streamer.py
@@ -1,21 +1,21 @@
-from realtime_audio_streamer import RealtimeAudioStreamer
+# from realtime_audio_streamer import RealtimeAudioStreamer
 from localization import SoundLocalizer
 from models.configs.localization import LocalizationConfig
-from models.configs.realtime_streamer import RealtimeAudioStreamerConfig
+# from models.configs.realtime_streamer import RealtimeAudioStreamerConfig
 
 def main() -> None:
     localizer = SoundLocalizer(config=LocalizationConfig())
-    with RealtimeAudioStreamer(config=RealtimeAudioStreamerConfig()) as streamer:
-        return
-        for audio_streams in streamer.audio_generator():
-            try:
-                if audio_streams is None:
-                    print("Audio streams are None")
-                    continue
+    # with RealtimeAudioStreamer(config=RealtimeAudioStreamerConfig()) as streamer:
+    #     return
+    #     for audio_streams in streamer.audio_generator():
+    #         try:
+    #             if audio_streams is None:
+    #                 print("Audio streams are None")
+    #                 continue
 
-                print("Got audio!")
-            except Exception as e:
-                print(f"Realtime audio error: {e}")
+    #             print("Got audio!")
+    #         except Exception as e:
+    #             print(f"Realtime audio error: {e}")
 
 
 if __name__ == "__main__":
