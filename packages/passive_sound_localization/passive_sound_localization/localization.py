fix: correcting movement_library rotation and choosing best angle (#30)
* feat: Added explicit calculation that calculates the localization angle based on the best direction and the front microphone

* doc: Added clarifying comments in movement library code

* doc: Added comment about speed variables and their units

* fix: Reset angular velocity after robot movement and adjust the angular speed based on the angle given

---------

Co-authored-by: John Finberg <johnbfinberg@gmail.com>

packages/passive_sound_localization/passive_sound_localization/localization.py
@@ -178,7 +178,7 @@ class SoundLocalizer:
             )
             if best_direction is not None:
                 # Convert direction into an angle for the result
-                estimated_angle = self._calculate_estimated_angle(best_direction)
+                estimated_angle = self._calculate_estimated_angle(best_direction=best_direction)
 
                 # Append the localization result for the source
                 results.append(
@@ -257,6 +257,22 @@ class SoundLocalizer:
 
         # Return the points stacked as (x, y) pairs
         return np.column_stack((x.ravel(), y.ravel()))
+    
+    # TODO: Type hinting could be improved
+    def _calculate_estimated_angle(self, best_direction: Tuple[float, float]) -> float:
+        """
+        Calculate the estimated angle based on the best direction and the front microphone.
+
+        Args:
+            best_direction (Tuple[float, float]): A tuple representing the x and y coordinates of the best direction.
+
+        Returns:
+            float: The estimated angle in degrees.
+        """
+        # The difference in x-coordinates is only the x-coordinate of best direction because the x-coordinate of the front microphone will always be zero
+        delta_x = best_direction[0]
+        delta_y = best_direction[1] - self.mic_positions[0, 1]
+        return np.degrees(np.atan2(delta_y, delta_x))
 
     def _calculate_estimated_angle(self, best_direction: Tuple[float, float]) -> float:
         delta_x = best_direction[0]